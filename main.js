d1a2588f76c186e63a11d7227c6d2d5e5a33876a
Enzo Pinheiro <oenzopinheiro@gmail.com>
2023-09-23 12:18:58 -0300

    Change on html and js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function calc() {
-    const valueDolar = parseFloat(document.querySelector(".input.name-dolar").value);
-    const stateFee = parseFloat(document.querySelector(".input.name-card").value);
+    const valueDolar = parseFloat(document.querySelector(".name-dolar").value);
+    const stateFee = parseFloat(document.querySelector(".name-card").value);
 
     let typeBuy;
     if (document.querySelector(".radioOne").checked) {
@@ -18,5 +18,39 @@ function calc() {
 
     const result = valueDolar * conversionRate;
 
-    document.querySelector(".value-result").textContent = `R$ ${result.toFixed(2)}`;
+    window.location.href=`/main.html?result=${result.toFixed(2)}`
+
+
+
 }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+window.onload = function () {
+    const result = getParameterByName("result");
+
+    if (result) {
+        document.querySelector(".value-result").textContent = `R$ ${result}`;
+    }
+};
+
+
+// async function getSingleCharacter(id) {
+//     fetch(`https://economia.awesomeapi.com.br/json/last/:moedas`)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((response) => {
+    
+//       });
+  
+  
+//   }
